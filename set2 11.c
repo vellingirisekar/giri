83be3a85da335ed837b1a42b10a4ea76656e405b
vellingirisekar <35956503+vellingirisekar@users.noreply.github.com>
2018-02-06 16:34:33 +0530

    Create set2 11.c

---
+++ set2 11.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter The Number\n");
+    scanf("%d",&a);
+    printf("Enter The Power\n");
+    scanf("%d",&b);
+    c=pow(a,b);
+    printf("The Value Is %d",c);
+  }
