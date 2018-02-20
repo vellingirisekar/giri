d83e130aa32a0aecdcf365ec1eb3f9151dfc50a1
vellingirisekar <35956503+vellingirisekar@users.noreply.github.com>
2018-02-20 15:49:19 +0530

    Create set1 16.c

---
+++ set1 16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+int n,m,i,j;
+scanf("%d,%d",&n,&m);
+for(i=n;i<m;i++)
+{ int count=0;
+for(j=2;j<i;j++)
+{
+if(i%j==0)
+{
+count++;
+}
+}
+if(count==0)
+{
+printf("%d\n",i);
+}
+}
+return 0;
+}
