808112cc1430a10d2a4713d3dd63658b94264e18
vellingirisekar <35956503+vellingirisekar@users.noreply.github.com>
2018-03-16 16:17:07 +0530

    Create set1 28.c

---
+++ set1 28.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,n;
+printf("enter the limimt");
+scanf("%d",&n);
+printf("enter the element");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("the elements with index");
+for(i=0;i<n;i++)
+{
+printf("%d\t%d\n",i,a[i]);
+}
+}
